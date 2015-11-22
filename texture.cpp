04c2e2b8c1c0c60a7312bc0cbb7b82ca91f3ad2b
Alex <asrutan@gmail.com>
2015-11-22 14:00:56 -0500

    Added Texture class

---
+++ texture.cpp
@@ -0,0 +1,16 @@
+// texture.cpp
+// Alex Rutan
+// 11/22/15
+#include <iostream>
+#include "texture.h"
+
+using namespace std;
+
+Texture::Texture()
+{
+    ground = 500;
+} //end constructor
+
+Texture::~Texture()
+{
+} //end destructor
---
+++ texture.h
@@ -0,0 +1,18 @@
+// texture.h
+// Alex Rutan
+// 11/22/15
+
+#ifndef TEXTURE_EXIST
+#define TEXTURE_EXIST
+
+class Texture
+{
+    private:
+        int ground;
+
+    public:
+        Texture();
+        ~Texture();
+}; //end Texture
+
+#endif //TEXTURE_EXISTS
